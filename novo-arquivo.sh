6c983949b355c76f4585d717b1b9c8404f742eb5
Tom Hudson <mail@tomnomnom.com>
2024-12-05 18:34:52 -0300

    Teste de upload de arquivos na branch

---
+++ novo-arquivo.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# MAUVADAO
+# VER: 1.0.0
+
+
+echo -e "\e[1;33mTestes de criação de branchs\e[0m"
+
+        read -p "Qual o seu nome?: " nome
+
+case $nome in
+    teste1 )
+        echo  "Teste 1 agendados";;
+esac
+
+
+
