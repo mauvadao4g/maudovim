267b0f5006aad772d51ecf9708783ce71a02426d
Tom Hudson <mail@tomnomnom.com>
2024-12-07 09:59:13 -0300

    Update: 071224_09:59

--- subGit.sh
+++ subGit.sh
@@ -3,34 +3,6 @@
 # Script para adicionar, commitar e enviar alterações ao Git
 # Versão: 1.0.1
 
-
-new() {
-    # Obtém o último arquivo correspondente ao padrão "ver[0-9]*"
-    file=$(ls ver[0-9]* 2>/dev/null | sort -V | tail -n 1)
-
-    # Se não houver nenhum arquivo correspondente, inicia do zero
-    if [[ -z "$file" ]]; then
-        num=1
-    else
-        # Extrai o número da versão do arquivo existente
-        num=$(echo "$file" | grep -Eo '[0-9]+')
-        # Incrementa o número
-        ((num++))
-    fi
-
-    # Cria o novo nome do arquivo
-    newFile="ver$num"
-    # Deleta o file antigo
-    [[ -f $newFile ]] && { rm $file >/dev/null 2>&1; }
-    # Cria o novo arquivo
-    touch "$newFile"
-    echo "Novo arquivo criado: $newFile"
-}
-
-# Chama a função
-new
-
-
 # Função para exibir mensagens coloridas
 msg() {
     local color=$1
@@ -74,4 +46,3 @@ git config --global --add safe.directory "$DIR" && git push || { msg red "Erro a
 
 # Mensagem de sucesso
 msg green "Processo concluído com sucesso!"
-
