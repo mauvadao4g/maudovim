85fd3067b30843a3640e452478b6d7c5da17bfeb
Everton Rozendo <mauvadao4g@gmail.com>
2024-12-19 20:41:20 -0300

    191224_20:41
    ==========================
    maudovim
    Ver: 58
    Data: 2024-12-19 20:41:20
    Update: New Update
    ==========================

---
+++ vimrc_bkp/html_snippet/ligar.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# MAUVADAO
+# VER: 1.0.0
+
+
+echo -e "\e[1;33mLigando o Web Server com Python\e[0m"
+echo -e "\e[1;36m...\e[0m"
+echo -e "\e[1;34mSite: \e[1;33mhttp://localhost:3000\e[0m"
+
+# Ligando o web server
+python -m http.server 3000
+
+
