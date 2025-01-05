cb4cc176ba1ce0f583c92ed1090122f6b5fb563a
Everton Rozendo <mauvadao4g@gmail.com>
2025-01-05 16:36:56 -0300

    050125_16:36
    ==========================
    maudovim
    Ver: 153
    Data: 2025-01-05 16:36:56
    Update: New Update
    ==========================

--- snip-dump.sh
+++ snip-dump.sh
@@ -10,7 +10,7 @@
 	 # cp sh.snippets "$HOME/.vim/autoload/plug/start/vim-snippets/UltiSnips/sh.snippets"
        cp sh.snippets "$snip"
        [[ $? == 0 ]] && { echo "sh.snippets: configurado com sucesso"; } || {
-       echo -e "\e[1;31m[❌]ERROR AO COPIAR SNIPPETS[❌]" 
+       echo -e "\e[0;31m[❌]ERROR AO COPIAR SNIPPETS[❌]" 
        }
  
      }
@@ -19,11 +19,11 @@
 
 
 	 # CONFIGURANDO HTML.SNIPPETS
-    html_snip="$HOME/.vim/autoload/plug/start/vim-snippets/UltiSnips/sh.snippets"
+    html_snip="$HOME/.vim/autoload/plug/start/vim-snippets/UltiSnips/html.snippets"
      [[ -f "$html_snip" ]] && {
        # copiando o snippets pras configurações do vim
 #	 cp html.snippets "$HOME/.vim/autoload/plug/start/vim-snippets/UltiSnips/html.snippets"
-        cp sh.snippets "$html_snip"
+        cp html.snippets "$html_snip"
        [[ $? == 0 ]] && { echo "html.snipeets: configurado com sucesso"; } || {
        echo -e "\e[1;31m[❌]ERROR AO COPIAR SNIPPETS[❌]" 
        }
