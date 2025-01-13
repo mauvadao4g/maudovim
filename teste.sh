e76e5a7497fbaa523ca2c3e45b014a2b6bcff6cd
Everton Rozendo <mauvadao4g@gmail.com>
2025-01-12 23:18:27 -0300

    120125_23:18
    ==========================
    maudovim
    Ver: 7
    Data: 2025-01-12 23:18:27
    Update: New Update
    ==========================

--- teste.sh
+++ teste.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-# MAUVADAO
-# VER: 1.0.0
-
-echo 'upando arquivo para o git'
-echo 'linha 2 upando para o github'
-
-echo 'Teste ....'
-
-
-
-
-
-
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+# COMENTARIO
+echo 'Testando ....'
