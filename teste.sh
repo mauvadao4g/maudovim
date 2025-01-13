b677e7499639e23331d544cc79cd600de9ee41b7
Everton Rozendo <mauvadao4g@gmail.com>
2025-01-12 23:19:43 -0300

    120125_23:19
    ==========================
    maudovim
    Ver: 8
    Data: 2025-01-12 23:19:43
    Update: New Update
    ==========================

--- teste.sh
+++ teste.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
+echo "Buscando lista de dominios do Ip "
+echo "Digite o Ip: "
+read ip
+echo "Digite o nome do arquivo: "
+read arquivo
+echo "Digite o nome do arquivo de saida: "
+read saida
+echo "Buscando lista de dominios do Ip $ip"
+
+for i in $(cat $arquivo); do
+host $i $ip | grep "has address" | cut -d " " -f1,4 >> $saida
+done
+
+echo "Lista de dominios do Ip $ip salva no arquivo $saida"
+
+	
+# Fim do script
+# Autor: Eder Queiroz
+# 	
+# 		
 #
-#
-#
-#
-#
-#
-#
-#
-#
-#
-# COMENTARIO
-echo 'Testando ....'
